b4e5b14d005012a57d3cd331892652e9c7d8b0c5
Chroz <Chroz@DESKTOP-NCK3OP5>
2017-08-30 08:24:31 +0200

    Filled out a DBInitializer that dropcreates the database if the model changes.

--- Tourney.DataAccess/TourneyDBInitializer.cs
+++ Tourney.DataAccess/TourneyDBInitializer.cs
@@ -1,4 +1,9 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Tourney.Model;
+using Tourney.Model.LocationClasses;
+using Tourney.Model.ParticipantClasses;
 
 namespace Tourney.DataAccess
 {
@@ -6,7 +11,31 @@ namespace Tourney.DataAccess
     {
         protected override void Seed(TourneyContext context)
         {
-            // Add test data.
+            // Test the model
+            var continent = new Continent("Europe");
+            var country = new Country("Norway", continent);
+            var region = new Region("Østfold", country);
+            var city = new City("Halden", region);
+            continent.Countries.Add(country);
+            country.Regions.Add(region);
+            region.Cities.Add(city);
+
+            var location = new Location() { Continent = continent, Country = country, Region = region, City = city };
+
+            var person = new Person("Torstein", "Røsok")
+            {
+                DateOfBirth = new DateTime(1993, 04, 23),
+                Nationality = location,
+                Residence = location,
+                Players = new List<Player>()
+            };
+
+            context.Continents.Add(continent);
+            context.Countries.Add(country);
+            context.Regions.Add(region);
+            context.Cities.Add(city);
+            context.Locations.Add(location);
+            context.People.Add(person);
 
             base.Seed(context);
         }
